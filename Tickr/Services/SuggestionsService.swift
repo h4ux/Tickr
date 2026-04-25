92a3538ab1ba3905230dc9596ddf07240965ff18
h4ux <h4ux@users.noreply.github.com>
2026-04-25 14:16:44 +0300

    new tickers category

--- Tickr/Services/SuggestionsService.swift
+++ Tickr/Services/SuggestionsService.swift
@@ -39,7 +39,7 @@ class SuggestionsService: ObservableObject {
         {"name":"Gaming","icon":"gamecontroller","symbols":["ATVI","EA","TTWO","RBLX","U"]},
         {"name":"Travel & Airlines","icon":"airplane","symbols":["DAL","UAL","LUV","AAL","ABNB","BKNG"]},
         {"name":"Retail","icon":"cart","symbols":["WMT","COST","TGT","HD","LOW","AMZN"]},
-        {"name":"Space","icon":"moon.stars","symbols":["RKLB","LUNR","ASTS","PL","IRDM","SPCE","BKSY","RDW"]}
+        {"name":"Space","icon":"moon.stars","symbols":["RKLB","LUNR","ASTS","PL","IRDM","SPCE","BKSY","RDW","LMT","BA","NOC","LHX"]}
         ]}
         """
 
